f9020e9fe1d5c449adecfc77ea6e5f6bdd61bdbc
KelfinTDetonator <kelfinantoni@gmail.com>
2023-06-28 01:31:28 +0700

    modify css grid, styling search bar, font

--- app.js
+++ app.js
@@ -16,14 +16,17 @@ form.addEventListener('submit', async(e) => {
 })
 
 const makeImgs = (shows) => {
-    container.innerText = ''; // remove all previous images
+    container.innerText = ''; // hapus semua gambar dari hasil search sebelumnya
     for(let result of shows){
-        if(result.show.image) {
+        if(result.show.image) { //Jika terdapat gambar pada API, maka 
             const div = document.createElement('DIV');
             const img = document.createElement('IMG');
-            
+            const div2 = document.createElement ('div');
+            div2.textContent = result.show.name;
+            div2.classList.add('title')
             img.src = result.show.image.medium;
             div.append(img);
+            div.append(div2);
             container.append(div);
         }
     }
