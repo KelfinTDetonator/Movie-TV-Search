c427ec11cd0b016cbf496e215ba26ee2c929d64b
KelfinTDetonator <kelfinantoni@gmail.com>
2023-06-29 02:51:38 +0700

    menambahkan link official, adjust grid

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#searchFrom')
 const input = document.querySelector('input');
 let container = document.querySelector('.grid');
+// let site = document.querySelector('.site');
 form.addEventListener('submit', async(e) => {
     e.preventDefault();
     let searchTerm = input.value;
@@ -21,12 +22,33 @@ const makeImgs = (shows) => {
         if(result.show.image) { //Jika terdapat gambar pada API, maka 
             const div = document.createElement('DIV');
             const img = document.createElement('IMG');
-            const div2 = document.createElement ('div');
+            const div2 = document.createElement('div');
+            const intro = document.createElement('div');
+            const linkIntro = document.createElement('a');
+            const headIntro = document.createElement('h3')
             div2.textContent = result.show.name;
-            div2.classList.add('title')
+            div.classList.add('card')
+            div2.classList.add('title');
+            intro.classList.add('intro');
+
+            linkIntro.href = result.show.officialSite;
+            headIntro.innerHTML = "Official Site >";
+            linkIntro.append(headIntro);
+            intro.append(linkIntro);  
+           
+            // site.href = result.show.officialSite;
+           
+            // img.append(site);
+
             img.src = result.show.image.medium;
             div.append(img);
             div.append(div2);
+            div2.append(intro);
+           
+            // div.append(div2);
+            // div2.append(intro);
+            
+
             container.append(div);
         }
     }
